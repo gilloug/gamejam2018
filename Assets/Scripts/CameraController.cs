824fa7bdb06861624d742b8de6f19532b302adb1
gillou_g <guillaume.gilloux@epitech.eu>
2018-01-27 19:09:26 +0100

    .

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject[] players;
+	private Camera cam;
+
+	void Start () {
+		cam = GetComponent<Camera> ();
+	}
+
+	void Update () {
+		Vector3 v = new Vector3 ();
+		foreach (GameObject player in players) {
+			v += player.transform.position;
+		}
+		v /= players.Length;
+		cam.transform.LookAt (v);
+		float dist = 0;
+		float new_dist = 0;
+		foreach (GameObject player in players) {
+			new_dist = Mathf.Abs (player.transform.position.x - v.x);
+			if (new_dist > dist)
+				dist = new_dist;
+			new_dist = Mathf.Abs (player.transform.position.y - v.y);
+			if (new_dist > dist)
+				dist = new_dist;
+		}
+		cam.orthographicSize = 1 + dist;
+	}
+}
---
+++ Assets/Scripts/DontGoThroughThings.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class DontGoThroughThings : MonoBehaviour
+{
+	private LayerMask layerMask;
+	private Vector2 previousPosition; 
+	private Rigidbody2D myRigidbody;
+ 
+	//initialize values 
+	void Start() {
+		myRigidbody = GetComponent<Rigidbody2D>();
+		previousPosition = (Vector2)myRigidbody.position; 
+		layerMask = 1 << LayerMask.NameToLayer ("Wall");
+	}
+ 
+	void FixedUpdate() {
+		Vector2 movementThisStep = (Vector2)myRigidbody.position - previousPosition;
+		RaycastHit2D hitInfo = Physics2D.Raycast(previousPosition, movementThisStep, movementThisStep.magnitude, layerMask.value);
+		if (hitInfo.collider) {
+			Debug.Log (previousPosition);
+			Debug.Log(hitInfo.point);
+			Debug.Log ("---------------");
+			myRigidbody.velocity = new Vector2 ();
+			myRigidbody.position = previousPosition;
+		} else {
+			previousPosition = myRigidbody.position;
+		}
+	}
+}
